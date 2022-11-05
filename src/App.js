6f8b3960a65c8e5fe83729337acdf1e7c66a8b44
WeWantSomeMo <mason.oz5@yahoo.com>
2022-11-05 08:56:49 -0500

    React Test

---
+++ src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import NavbarSetup from './components/NavbarSetup';
+
+function App() {
+  return (
+    <div className="App">
+      <NavbarSetup  />
+    </div>
+  );
+}
+
+export default App;

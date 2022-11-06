f67ae4681b88906d53cd5e7329667c96b92037b3
DT1220 <104799954+DT1220@users.noreply.github.com>
2022-11-05 23:30:51 -0500

    Updated Queries

---
+++ Client/src/components/QueryList.js
@@ -0,0 +1,27 @@
+import {GQL} from 'apollo-boost';
+import { Component } from 'react';
+import {graphql} from 'react-apollo';
+
+const getQuery = gql`
+{
+    savings {
+        name
+        amount
+    }
+}`
+
+
+class QueryList extends Component {
+    render() {
+        return (
+            <div>
+                <ul id="query-list">
+                    <li> Query Name</li>
+                </ul>
+            </div>
+        )
+    }
+}
+
+
+export default graphql(getQuery)(QueryList);
